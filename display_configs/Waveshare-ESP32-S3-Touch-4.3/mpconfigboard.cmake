eb4dd13fb6539b35b8d7a080420c7736a82e0a43
alex_netz <netz_alex@web.de>
2025-12-22 21:49:12 +0100

    delete sdkconfig.usb from mpconfigboard.cmake

--- display_configs/Waveshare-ESP32-S3-Touch-4.3/mpconfigboard.cmake
+++ display_configs/Waveshare-ESP32-S3-Touch-4.3/mpconfigboard.cmake
@@ -3,7 +3,6 @@ set(IDF_TARGET esp32s3)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     ${SDKCONFIG_IDF_VERSION_SPECIFIC}
-    boards/sdkconfig.usb
     boards/sdkconfig.ble
     boards/sdkconfig.spiram_sx
     boards/sdkconfig.spiram_oct
@@ -14,4 +13,5 @@ set(SDKCONFIG_DEFAULTS
 list(APPEND MICROPY_DEF_BOARD
     MICROPY_HW_BOARD_NAME="ESP32_S3_Waveshare4.3"
 )
+
 
